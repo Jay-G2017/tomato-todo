1b7ff53124a1036b0eab5fe91eec0932c6494608
jay.gao <jay.gao@alo7.com>
2019-04-12 16:48:45 +0800

    feat: 101

---
+++ src/components/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./todo.scss";
+
+class Todo extends Component {
+  state = {};
+  render() {
+    return <div className="todo-list">todo</div>;
+  }
+}
+
+export default Todo;
